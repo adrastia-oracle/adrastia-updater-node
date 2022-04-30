6dde35590207d7e2a54d2f0220ad9624a46cec04
Tyler Loewen <tyler@trilez.com>
2022-04-29 18:23:59 -0700

    Upgrade pythia-core to v1.0.0-rc.8 - The oracle address changes to one which uses this version of pythia-core

--- pythia.config.js
+++ pythia.config.js
@@ -21,7 +21,7 @@ export default {
             oracles: [
                 {
                     enabled: true,
-                    address: "0x0b247F8A15dA3D013747493A3E0C8A3E7edeA717", // Aggregated oracle USDC
+                    address: "0xC8a4609C18ca3628676C0478779037898ab498f4", // Aggregated oracle USDC
                     tokens: [
                         "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", // WETH
                         "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", // WMATIC
