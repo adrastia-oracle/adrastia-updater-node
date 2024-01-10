2f5e4af71b012eb35af9112854a4ef33caf01977
Tyler Loewen <tyler@trilez.com>
2024-01-09 18:03:38 -0800

    Add Multicall2 deployment script

---
+++ scripts/deploy-multicall2.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const factory = await ethers.getContractFactory("Multicall2");
+    const contract = await factory.deploy();
+
+    await contract.waitForDeployment();
+
+    console.log("Multicall2 deployed to:", contract.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
