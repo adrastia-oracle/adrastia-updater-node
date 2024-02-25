f74eb2efc81a98e0a5cfbebd01f75c0cdfc36be4
Tyler Loewen <tyler@trilez.com>
2024-02-24 18:03:15 -0800

    Add revert support in MockAutomationAci

--- contracts/MockAutomationAci.sol
+++ contracts/MockAutomationAci.sol
@@ -9,23 +9,41 @@ contract MockAutomationAci is AutomationCompatibleInterface {
         bytes performData;
         uint256 orderBlockNumber;
         uint256 fulfillBlockNumber;
+        bool customRevertInPerform;
+        bool stringRevertInPerform;
+        bool emptyRevertInPerform;
+        bool outOfGasRevertInPerform;
     }
 
     mapping(address => Upkeep) public upkeep;
 
+    uint256 internal junk;
+
     event UpkeepPerformed(bytes performData, uint256 blockDelay);
-    
-    function orderNewWork(bytes calldata performData) external {
+
+    error CustomRevertInPerform();
+
+    function orderNewWork(
+        bytes calldata performData,
+        bool customRevertInPerform,
+        bool stringRevertInPerform,
+        bool emptyRevertInPerform,
+        bool outOfGasRevertInPerform
+    ) external {
         address token = abi.decode(performData, (address));
 
         upkeep[token] = Upkeep({
             upkeepNeeded: true,
             performData: performData,
             orderBlockNumber: block.number,
-            fulfillBlockNumber: 0
+            fulfillBlockNumber: 0,
+            customRevertInPerform: customRevertInPerform,
+            stringRevertInPerform: stringRevertInPerform,
+            emptyRevertInPerform: emptyRevertInPerform,
+            outOfGasRevertInPerform: outOfGasRevertInPerform
         });
     }
-    
+
     function checkUpkeep(bytes calldata checkData) external view override returns (bool, bytes memory) {
         address token = abi.decode(checkData, (address));
 
@@ -33,7 +51,7 @@ contract MockAutomationAci is AutomationCompatibleInterface {
 
         return (_upkeep.upkeepNeeded, _upkeep.performData);
     }
-    
+
     function performUpkeep(bytes calldata petformData) external override {
         address token = abi.decode(petformData, (address));
 
@@ -42,6 +60,15 @@ contract MockAutomationAci is AutomationCompatibleInterface {
         require(keccak256(_upkeep.performData) == keccak256(petformData), "petformData must match");
         require(_upkeep.upkeepNeeded, "no upkeep needed");
         require(_upkeep.fulfillBlockNumber == 0, "upkeep already fulfilled");
+        if (_upkeep.customRevertInPerform) revert CustomRevertInPerform();
+        require(!_upkeep.stringRevertInPerform, "string revert in perform");
+        require(!_upkeep.emptyRevertInPerform);
+
+        if (_upkeep.outOfGasRevertInPerform) {
+            for (uint i = 0; i < type(uint256).max; i++) {
+                junk = i;
+            }
+        }
 
         _upkeep.upkeepNeeded = false;
         _upkeep.fulfillBlockNumber = block.number;
--- scripts/order-work-mock-automation-aci.ts
+++ scripts/order-work-mock-automation-aci.ts
@@ -1,16 +1,27 @@
 import { ethers } from "hardhat";
 
-const CONTRACT_ADDRESS = "0x067f1B281E8bc66c9CC9D7922120f753FC25B10c";
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 const TOKEN = "0x0000000000000000000000000000000000000000";
 const DATA = "0x00";
+
+const customRevertInPerform = false;
+const stringRevertInPerform = false;
+const emptyRevertInPerform = false;
+const outOfGasRevertInPerform = false;
 
 async function main() {
     const contract = await ethers.getContractAt("MockAutomationAci", CONTRACT_ADDRESS);
 
     const performData = await ethers.AbiCoder.defaultAbiCoder().encode(["address", "bytes"], [TOKEN, DATA]);
 
-    await contract.orderNewWork(performData);
+    await contract.orderNewWork(
+        performData,
+        customRevertInPerform,
+        stringRevertInPerform,
+        emptyRevertInPerform,
+        outOfGasRevertInPerform,
+    );
 
     console.log("Work order sent.");
 }
