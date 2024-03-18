6fa68b7c70831eae59435d463eb46689e65dacde
Tyler Loewen <tyler@trilez.com>
2024-03-18 15:48:55 -0700

    Allow restarting services by chain

--- services/scripts/restart-services.sh
+++ services/scripts/restart-services.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-for service in $(systemctl list-units --type=service --state=loaded --all | grep 'adrastia-' | awk '{print $1}'); do
+
+# Prefix
+prefix="adrastia-"
+
+# Check for optional 'chain' argument
+if [ ! -z "$1" ]; then
+  prefix="${prefix}$1-"
+fi
+
+for service in $(systemctl list-units --type=service --state=loaded --all | grep "^$prefix" | awk '{print $1}'); do
     if systemctl is-enabled --quiet "$service"; then
         echo "Restarting enabled service: $service"
         systemctl restart "$service"
